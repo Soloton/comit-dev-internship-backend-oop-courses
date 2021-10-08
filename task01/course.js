195a1c7fce3dc263eed3d1730811673a0aac0f51
Mikhail Arkhipov <arkhipov.ms@gmail.com>
2021-10-09 01:10:11 +0300

    refactor(develop): Clean code

--- task01/course.js
+++ task01/course.js
@@ -4,20 +4,12 @@ export class Course extends Topic {
   topics = new Set();
 
   add(topic) {
-    if (this.topics === undefined) {
-      this.topics = new Set();
-    }
-
     if (!this.topics.has(topic)) {
       this.topics.add(topic);
     }
   }
 
   delete(topic) {
-    if (this.topics === undefined) {
-      this.topics = new Set();
-    }
-
     if (this.topics.has(topic)) {
       this.topics.delete(topic);
     }
--- task01/topic.js
+++ task01/topic.js
@@ -4,20 +4,12 @@ export class Topic extends Base {
   answers = [];
 
   add(answer) {
-    if (this.answers === undefined) {
-      this.answers = [];
-    }
-
     if (this.answers.indexOf(answer) === -1) {
       this.answers.push(answer);
     }
   }
 
   delete(answer) {
-    if (this.answers === undefined) {
-      this.answers = [];
-    }
-
     const indexOfAnswer = this.answers.indexOf(answer);
     if (indexOfAnswer > -1) {
       this.answers.splice(indexOfAnswer, 1);
