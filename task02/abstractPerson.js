2dbf398b36890d0b914bbb454baede75bd4b28da
Mikhail Arkhipov <arkhipov.ms@gmail.com>
2021-11-02 21:23:52 +0300

    feat(task005): Change AbstractPerson to es5 syntax

--- task02/abstractPerson.js
+++ task02/abstractPerson.js
@@ -1,27 +1,25 @@
 import { Goods } from "./goods.js";
 
-export class AbstractPerson {
-  _goods = new Goods(0);
-
-  constructor() {
-    if (this.constructor === AbstractPerson) {
-      throw new TypeError("Can not construct abstract class.");
-    }
+export function AbstractPerson() {
+  if (this.constructor === AbstractPerson) {
+    throw new TypeError("Can not construct abstract class.");
   }
 
-  setGoodsCount(goodsCount) {
-    this._goods.setCount(goodsCount);
-  }
-
-  increaseGoodsCount(goodsCount) {
-    return this._goods.increaseCount(goodsCount);
-  }
-
-  decreaseGoodsCount(goodsCount) {
-    return this._goods.decreaseCount(goodsCount);
-  }
-
-  getGoodsCount() {
-    return this._goods.getCount();
-  }
+  this._goods = new Goods(0);
 }
+
+AbstractPerson.prototype.setGoodsCount = function (goodsCount) {
+  this._goods.setCount(goodsCount);
+};
+
+AbstractPerson.prototype.increaseGoodsCount = function (goodsCount) {
+  return this._goods.increaseCount(goodsCount);
+};
+
+AbstractPerson.prototype.decreaseGoodsCount = function (goodsCount) {
+  return this._goods.decreaseCount(goodsCount);
+};
+
+AbstractPerson.prototype.getGoodsCount = function () {
+  return this._goods.getCount();
+};
